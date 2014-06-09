9c4774117e87e6e6b11160f0c6134bc70c214fab
Kieran Simpson <kierans777@gmail.com>
2014-06-09 10:15:04 +1000

    Fixed test suite description.

--- examples/example-test.js
+++ examples/example-test.js
@@ -4,7 +4,7 @@
 
 var should = require("mocha-should");
 
-describe("Node constructor tests", function() {
+describe("Samples tests", function() {
   should("have true be true", function() {
     expect(true).to.equal(true);
   });
